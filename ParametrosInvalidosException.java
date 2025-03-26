181217618e552bb269f7c9cee4eae90a7405872b
Leonardo Felipe Ribas Ferreira <Leonardo_felipe1@outlook.com>
2025-03-26 17:04:32 -0300

    Delete ParametrosInvalidosException.java

--- ParametrosInvalidosException.java
+++
@@ -1,8 +0,0 @@
-// ParametrosInvalidosException.java
-public class ParametrosInvalidosException extends Exception {
-    
-    // Construtor que recebe uma mensagem
-    public ParametrosInvalidosException(String mensagem) {
-        super(mensagem);  // Passa a mensagem para a classe pai (Exception)
-    }
-}
